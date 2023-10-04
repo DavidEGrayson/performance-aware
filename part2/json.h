d54d353c25069ece93a9b86bc57fe0bbb010072f
David Grayson <davidegrayson@gmail.com>
2023-10-03 20:06:25 -0700

    Make it much faster by doing one fread.
    pairs: 10000
    average: 6932.907748145240475
    Total run time:                      64058 us
      JSON parse                  1      58120 us          0 us ( 0.0%)
      json_parse_file             1      58120 us         22 us ( 0.0%)
      jpf - fread                 1       1346 us       1346 us ( 2.1%) 0.69 GiB/s
      json_parse_core        180006      56751 us      14039 us (21.9%)
      jpc - skip spaces      180006       1810 us       1810 us ( 2.8%)
      jpc - object            10001      56750 us       4288 us ( 6.7%)
      jpc - string            40001       5953 us       5953 us ( 9.3%)
      jpc - array                 1      56748 us        541 us ( 0.8%)
      jpc - number            40000      30117 us       4123 us ( 6.4%)
      jpc - strtod            40000      25994 us      25994 us (40.6%)
      Look up pairs               1          0 us          0 us ( 0.0%)
      Average                     1       4167 us       4167 us ( 6.5%) 0.07 GiB/s
      Print results               1       1726 us       1726 us ( 2.7%)

--- part2/json.h
+++ part2/json.h
@@ -26,51 +26,50 @@ typedef struct Json
   struct Json * next;
 } Json;
 
+typedef struct JsonInputBuffer
+{
+  size_t index;
+  size_t size;
+  char * data;
+} JsonInputBuffer;
+
 bool json_is_value(enum JsonType type)
 {
   return type >= JsonObject && type <= JsonString;
 }
 
-char next_char(FILE * file)
+char next_char(JsonInputBuffer * buf)
 {
-  profile_block("next_char");
-  char c;
-  size_t result = fread(&c, 1, 1, file);
-  assert(result == 1);
-  profile_record_bytes(1);
-  profile_block_done();
-  return c;
+  if (buf->index >= buf->size) { buf->index = buf->size - 1; }
+  return buf->data[buf->index++];
 }
 
-void unread_char(FILE * file)
+void unread_char(JsonInputBuffer * buf)
 {
-  profile_block("uread_char");
-  fseek(file, -1, SEEK_CUR);
-  profile_record_bytes(1);
-  profile_block_done();
+  if (buf->index) { buf->index--; }
 }
 
-Json * json_parse_file(FILE * file)
+Json * json_parse_core(JsonInputBuffer * buf)
 {
-  profile_block("json_parse_file");
+  profile_block("json_parse_core");
   Json * ret = calloc(sizeof(Json), 1);
   char c;
-  profile_block("jpf - skip spaces");
+  profile_block("jpc - skip spaces");
   do
   {
-    c = next_char(file);
+    c = next_char(buf);
   }
   while (c == ' ' || c == '\n');
   profile_block_done();
   if (c == '"')
   {
-    profile_block("jpf - string");
+    profile_block("jpc - string");
     ret->type = JsonString;
     ret->string = malloc(256);
     char * p = ret->string;
     while (true)
     {
-      c = next_char(file);
+      c = next_char(buf);
       if (c == '"') { break; }
       // WARNING: Buffer overflow below if strings are too long.
       *p++ = c;
@@ -80,7 +79,7 @@ Json * json_parse_file(FILE * file)
   }
   else if (c == '-' || (c >= '0' && c <= '9'))
   {
-    profile_block("jpf - number");
+    profile_block("jpc - number");
     ret->type = JsonNumber;
     // WARNING: buffer overflow below if floats are too long
     char float_string[256];
@@ -88,31 +87,31 @@ Json * json_parse_file(FILE * file)
     float_string[0] = c;
     while (true)
     {
-      c = next_char(file);
+      c = next_char(buf);
       if (c == '-' || (c >= '0' && c <= '9') || c == '.')
       {
         *p++ = c;
       }
       else
       {
-        unread_char(file);
+        unread_char(buf);
         break;
       }
     }
     *p = 0;
-    profile_block("jpf - strtod");
+    profile_block("jpc - strtod");
     ret->number = strtod(float_string, NULL);
     profile_block_done();
     profile_block_done();
   }
   else if (c == '{')
   {
-    profile_block("jpf - object");
+    profile_block("jpc - object");
     ret->type = JsonObject;
     Json ** tip = &ret->first;
     while (true)
     {
-      Json * first = json_parse_file(file);
+      Json * first = json_parse_core(buf);
       if (first->type == JsonObjectEnd)
       {
         free(first);
@@ -120,17 +119,17 @@ Json * json_parse_file(FILE * file)
       }
       *tip = first;
 
-      Json * colon = json_parse_file(file);
+      Json * colon = json_parse_core(buf);
       assert(colon->type == JsonColon);
       free(colon);
 
-      Json * value = json_parse_file(file);
+      Json * value = json_parse_core(buf);
       assert(json_is_value(value->type));
       first->next = value;
 
       tip = &value->next;
 
-      Json * last = json_parse_file(file);
+      Json * last = json_parse_core(buf);
       if (last->type == JsonObjectEnd)
       {
         free(last);
@@ -149,12 +148,12 @@ Json * json_parse_file(FILE * file)
   }
   else if (c == '[')
   {
-    profile_block("jpf - array");
+    profile_block("jpc - array");
     ret->type = JsonArray;
     Json ** tip = &ret->first;
     while (true)
     {
-      Json * element = json_parse_file(file);
+      Json * element = json_parse_core(buf);
       if (element->type == JsonArrayEnd)
       {
         free(element);
@@ -164,7 +163,7 @@ Json * json_parse_file(FILE * file)
       *tip = element;
       tip = &element->next;
 
-      Json * last = json_parse_file(file);
+      Json * last = json_parse_core(buf);
       if (last->type == JsonArrayEnd)
       {
         free(last);
@@ -204,6 +203,28 @@ Json * json_parse_file(FILE * file)
   }
   profile_block_done();
   return ret;
+}
+
+Json * json_parse_file(FILE * file)
+{
+  profile_block("json_parse_file");
+
+  fseek(file, 0, SEEK_END);
+  size_t file_size = ftell(file);
+  fseek(file, 0, SEEK_SET);
+  char * data = malloc(file_size);
+
+  profile_block("jpf - fread");
+  size_t bytes_read = fread(data, 1, file_size, file);
+  assert(bytes_read <= file_size);  // line ending conversions make these differ
+  profile_record_bytes(bytes_read);
+  profile_block_done();
+
+  JsonInputBuffer buf = { .size = bytes_read, .data = data };
+  Json * r = json_parse_core(&buf);
+  free(data);
+  profile_block_done();
+  return r;
 }
 
 Json * json_object_lookup(Json * obj, const char * name)
